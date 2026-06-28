4c1b64ede7f7b877a23d9c04ea670093a8f6c184
Raushan Kumar <rashukumar1504@gmail.com>
2026-06-28 23:56:22 +0100

    added background service worker

--- background.js
+++ background.js
@@ -1,25 +1,4 @@
-chrome.runtime.onMessage.addListener(data => {
-
-    const {event,prefs} = data;
-    switch (event) {
-        case 'onStop':
-            handleOnStop()
-            break;
-
-        case 'onStart':
-             handleOnStop(prefs)
-            break;
-
-        default:
-            break;
-    }
-});
-
-const handleOnStop = ()=>{
-    console.log("On Stop in the background")
-}
-
-const handleOnStop = (prefs)=>{
-     console.log("On Start in the background")
-}
-chrome.storage.local.set(prefs);
+ chrome.runtime.onMessage.addListener(data =>{
+    
+ }
+ )
--- popup/popup.js
+++ popup/popup.js
@@ -1,38 +1,20 @@
-// ELEMENTS
 const locationIdElement = document.getElementById("locationId");
 const startDateElement = document.getElementById("startDate");
 const endDateElement = document.getElementById("endDate");
 
-// Button elements
-const startButton = document.getElementById("startButton");
+const startButton = document.getElementById("startButton")
 const stopButton = document.getElementById("endButton");
 
-const getPrefs = () => ({
-    locationId: locationIdElement.value,
-    startDate: startDateElement.value,
-    endDate: endDateElement.value
-});
 
-startButton.onclick = () => {
-    chrome.runtime.sendMessage({ event: "onStart", prefs: getPrefs() });
-};
-
-stopButton.onclick = () => {
-    chrome.runtime.sendMessage({ event: "onStop", prefs: getPrefs() });
-};
-
-chrome.storage.local.get(["locationId","startDate","endDate"], (result) => {
-    const { locationId, startDate, endDate } = result;
-
-    if (locationId) {
-        locationIdElement.value = locationId;
+startButton.onclick = ()=> {
+    if(startDateElement.value){
+       console.log(" Valid start Date :", startDateElement.value)
+    }else{
+        console.log("Start Date invalid");
     }
+}
 
-    if (startDate) {
-        startDateElement.value = startDate;
-    }
+stopButton.onclick = ()=> {
+    console.log("You clicked the stop Button")
 
-    if (endDate) {
-        endDateElement.value = endDate;
-    }
-});
+}
